0135e6226dbdb630c346fa30ee8d32fae0fffd27
ArgentumGirl <ioiosha@proton.me>
2025-12-07 09:25:06 +0500

    Initial commit: - Current diagrams (UML and Chen) included - Partial database created in SSMS (Employee and Reader entities and related tables added) - WPF app structure created with basic authorization window - Database not yet connected to the app

---
+++ app/Library/Library/Wins/Authorization.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Library.Wins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Library
+{
+    /// <summary>
+    /// Логика взаимодействия для Authorization.xaml
+    /// </summary>
+    public partial class Authorization : Window
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+            this.Closing += Authorization_Closing;
+        }
+
+        private void Authorization_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ButtonLogin_Click(object sender, RoutedEventArgs e)
+        {
+            Role1Window role1win = new Role1Window();
+            this.Hide();
+            role1win.Show();
+        }
+    }
+}
---
+++ app/Library/Library/Wins/Role1Window.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Library.Wins
+{
+    /// <summary>
+    /// Логика взаимодействия для Role1Window.xaml
+    /// </summary>
+    public partial class Role1Window : Window
+    {
+        public Role1Window()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonLogOut_Click(object sender, RoutedEventArgs e)
+        {
+            Authorization authwin = new Authorization();
+            authwin.Show();
+            this.Close();
+        }
+    }
+}
